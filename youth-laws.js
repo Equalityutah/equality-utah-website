943beea81846e3423ec3c37125c1330f109f218b
Equalityutah <69677658+Equalityutah@users.noreply.github.com>
2020-08-14 05:42:02 -0600

    Create youth-laws.js

---
+++ youth-laws.js
@@ -0,0 +1,619 @@
+<script>
+
+<!-- ENUMERATED CATEGORIES IN LAW MODAL -->
+
+    $(function() {
+
+  $('.categories-law').click(function() {
+
+    $('.categories-law-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.categories-law-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+  
+  $('.modal-background').click(function() {
+
+      $('.categories-law-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.categories-law-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- ENUMERATED MODEL POLICY MODAL -->
+
+    $(function() {
+
+  $('.model-policy').click(function() {
+
+    $('.model-policy-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.model-policy-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+  
+  $('.modal-background').click(function() {
+
+      $('.model-policy-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.model-policy-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- ALTERNATIVE DISCIPLINE MODAL -->
+
+    $(function() {
+
+  $('.discipline').click(function() {
+
+    $('.discipline-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.discipline-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.discipline-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.discipline-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- CYBERBULLYING MODAL -->
+
+    $(function() {
+
+  $('.cyberbullying').click(function() {
+
+    $('.cyberbullying-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.cyberbullying-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.cyberbullying-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.cyberbullying-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- SCHOOL SUICIDE PREVENTION POLICIES REQUIRED MODAL -->
+
+    $(function() {
+
+  $('.suicide-prevention').click(function() {
+
+    $('.suicide-prevention-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.suicide-prevention-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.suicide-prevention-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.suicide-prevention-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- TRANSGENDER INCLUSION IN SPORTS MODAL -->
+
+    $(function() {
+
+  $('.transgender-sports').click(function() {
+
+    $('.transgender-sports-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.transgender-sports-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.transgender-sports-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.transgender-sports-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- PROTECTION FROM CONVERSION THERAPY MODAL -->
+
+    $(function() {
+
+  $('.conversion-therapy').click(function() {
+
+    $('.conversion-therapy-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.conversion-therapy-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.conversion-therapy-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.conversion-therapy-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- LAWS TO ADDRESS LGBTQ YOUTH HOMELESSNESS MODAL -->
+
+    $(function() {
+
+  $('.youth-homelessness').click(function() {
+
+    $('.youth-homelessness-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.youth-homelessness-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.youth-homelessness-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.youth-homelessness-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- LGBTQ INCLUSIVE SEX EDUCATION LAWS MODAL -->
+
+    $(function() {
+
+  $('.sex-education').click(function() {
+
+    $('.sex-education-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.sex-education-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.sex-education-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.sex-education-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- LGBTQ INCLUSIVE JUVENILE JUSTICE POLICIES MODAL -->
+
+    $(function() {
+
+  $('.juvenile-justice').click(function() {
+
+    $('.juvenile-justice-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.juvenile-justice-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.juvenile-justice-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.juvenile-justice-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- INEQUALITY IN AGE OF CONSENT FOR SAME-SEX COUPLES MODAL -->
+
+    $(function() {
+
+  $('.age-consent').click(function() {
+
+    $('.age-consent-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.age-consent-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.age-consent-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.age-consent-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- SCHOOL LAWS THAT CRIMINALIZE YOUTH MODAL -->
+
+    $(function() {
+
+  $('.criminalize-youth').click(function() {
+
+    $('.criminalize-youth-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.criminalize-youth-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.criminalize-youth-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.criminalize-youth-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- ANTI-BULLYING LAWS THAT PROHIBIT ENUMERATION MODAL -->
+
+    $(function() {
+
+  $('.prohibit-enumeration').click(function() {
+
+    $('.prohibit-enumeration-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.prohibit-enumeration-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.prohibit-enumeration-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.prohibit-enumeration-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+<!-- LAWS THAT RESTRICT INCLUSION OF LGBTQ TOPICS IN SCHOOLS -->
+
+    $(function() {
+
+  $('.restrict-topics').click(function() {
+
+    $('.restrict-topics-flex-holder').fadeIn();
+
+    $('.modal-background').fadeIn();
+
+    e.stopPropagation();
+
+  });
+
+  $('.close-modal').click(function() {
+
+      $('.restrict-topics-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  });  
+
+  $('.modal-background').click(function() {
+
+      $('.restrict-topics-flex-holder').fadeOut();
+
+      $('.modal-background').fadeOut();
+
+  }); 
+
+  $(document).keydown(function (event) {
+
+      if (event.keyCode == 27) {
+
+          $('.restrict-topics-flex-holder').fadeOut();
+
+          $('.modal-background').fadeOut();
+
+      }
+
+  });
+
+});
+
+</script>
